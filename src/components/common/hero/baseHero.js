4fb6dd85ec640a9a80dff2183eb6787deacebebd
Victor Tran <vicr12345@gmail.com>
2023-02-13 23:32:21 +1100

    Fix some mobile issues

--- src/components/common/hero/baseHero.js
+++ src/components/common/hero/baseHero.js
@@ -5,22 +5,25 @@ import Card from "./card.svg";
 import Container from "../../container";
 import React from "react";
 
-function BaseHero({children, cardStyle = 0, style, className}) {
-    let card;
+function BaseHero({children, cardStyle = 0, style, className = ""}) {
+    let card, cardClass;
 
     switch (cardStyle) {
         case 1:
-            card = <Card1 className={`${Styles.heroCard} ${Styles.heroCard1}`} />
+            card = <Card1 className={Styles.heroCard} />
+            cardClass = Styles.heroCard1;
             break;
         case 2:
-            card = <Card2 className={`${Styles.heroCard} ${Styles.heroCard2}`} />
+            card = <Card2 className={Styles.heroCard} />
+            cardClass = Styles.heroCard2;
             break;
         case 0:
         default:
-            card = <Card className={`${Styles.heroCard} ${Styles.heroCard0}`} />
+            card = <Card className={Styles.heroCard} />
+            cardClass = Styles.heroCard0;
     }
 
-    return <Container className={`${className} ${Styles.heroContainer}`} style={style}>
+    return <Container className={`${className} ${Styles.heroContainer} ${cardClass}`} style={style}>
         {card}
         <div className={Styles.heroItems}>
             {children}
